0857b80e427e95eaf404743bb39dab1e7e5d1143
Sovianum <aklyukvin@yandex.com>
2017-09-28 20:05:06 +0300

    formatted file

--- server/env.go
+++ server/env.go
@@ -1,28 +1,18 @@
 package server
 
 import (
-	"database/sql"
 	"github.com/Sovianum/acquaintanceServer/config"
 	"github.com/Sovianum/acquaintanceServer/dao"
+	"github.com/patrickmn/go-cache"
 )
 
 type tokenKeyGetterType func() string
 
 type Env struct {
-	userDAO       dao.UserDAO
-	positionDAO   dao.PositionDAO
-	authConf      config.AuthConfig
-	logicConf     config.LogicConfig
-	hashFunc      func(password []byte) ([]byte, error)
-	hashValidator func(password []byte, hash []byte) error
-}
-
-func NewEnv(db *sql.DB, authConf config.AuthConfig) *Env {
-	var result = &Env{
-		userDAO:     dao.NewDBUserDAO(db),
-		positionDAO: dao.NewDBPositionDAO(db),
-		authConf:    authConf,
-	}
-
-	return result
+	userDAO          dao.UserDAO
+	positionDAO      dao.PositionDAO
+	authConf         config.AuthConfig
+	logicConf        config.LogicConfig
+	hashFunc         func(password []byte) ([]byte, error)
+	hashValidator    func(password []byte, hash []byte) error
 }
