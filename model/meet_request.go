c0ad0ca3ebe2bb2066a8f5d3174b8a09c594b2ab
Sovianum <aklyukvin@yandex.com>
2017-09-28 23:53:18 +0300

    updated request model

--- model/meet_request.go
+++ model/meet_request.go
@@ -1,8 +1,15 @@
 package model
 
-type CommRequest struct {
+const (
+	STATUS_PENDING  = "STATUS_PENDING"
+	STATUS_ACCEPTED = "STATUS_ACCEPTED"
+	STATUS_DECLINED = "STATUS_DECLINED"
+)
+
+type MeetRequest struct {
 	Id          int        `json:"id"`
 	RequesterId int        `json:"requester_id"`
 	RequestedId int        `json:"requested_id"`
 	Time        QuotedTime `json:"time"`
+	Status      string
 }
