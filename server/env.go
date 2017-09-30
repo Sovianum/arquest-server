cd41d8a167aceada162edad427bbec3c17cd413c
Sovianum <aklyukvin@yandex.com>
2017-09-30 14:27:40 +0300

    added meetRequestDAO to env

--- server/env.go
+++ server/env.go
@@ -11,6 +11,7 @@ type tokenKeyGetterType func() string
 type Env struct {
 	userDAO          dao.UserDAO
 	positionDAO      dao.PositionDAO
+	meetRequestDAO   dao.MeetRequestDAO
 	conf             config.Conf
 	hashFunc         func(password []byte) ([]byte, error)
 	hashValidator    func(password []byte, hash []byte) error
