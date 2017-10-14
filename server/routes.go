b3fdcff61ab527d536ae569fe82072b68f1e5217
Sovianum <aklyukvin@yandex.com>
2017-10-15 01:00:14 +0300

    update routes

--- server/routes.go
+++ server/routes.go
@@ -7,14 +7,14 @@ import (
 
 func GetRouter(env *Env) *mux.Router {
 	var router = mux.NewRouter()
-	router.HandleFunc("/api/v1/auth/sign_up/", env.UserRegisterPost).Methods(http.MethodPost)
-	router.HandleFunc("/api/v1/auth/sign_in/", env.UserSignInPost).Methods(http.MethodPost)
-	router.HandleFunc("/api/v1/pos/get_neighbours/", env.UserGetNeighboursGet).Methods(http.MethodGet)
-	router.HandleFunc("/api/v1/pos/save_position/", env.UserSavePositionPost).Methods(http.MethodPost)
-	router.HandleFunc("/api/v1/comm/create_request/", env.CreateRequest).Methods(http.MethodPost)
-	router.HandleFunc("/api/v1/comm/get_requests/", env.GetRequests).Methods(http.MethodGet)
-	router.HandleFunc("/api/v1/comm/update_request/", env.UpdateRequest).Methods(http.MethodPost)
-	router.HandleFunc("/api/v1/comm/get_new_requests/", env.GetNewRequestsEvents).Methods(http.MethodGet)
+	router.HandleFunc("/api/v1/auth/register", env.UserRegisterPost).Methods(http.MethodPost)
+	router.HandleFunc("/api/v1/auth/login", env.UserSignInPost).Methods(http.MethodPost)
+	router.HandleFunc("/api/v1/user/position/neighbours", env.UserGetNeighboursGet).Methods(http.MethodGet)
+	router.HandleFunc("/api/v1/user/position/save", env.UserSavePositionPost).Methods(http.MethodPost)
+	router.HandleFunc("/api/v1/user/request/create", env.CreateRequest).Methods(http.MethodPost)
+	router.HandleFunc("/api/v1/user/request/all", env.GetRequests).Methods(http.MethodGet)
+	router.HandleFunc("/api/v1/user/request/update", env.UpdateRequest).Methods(http.MethodPost)
+	router.HandleFunc("/api/v1/user/request/new", env.GetNewRequestsEvents).Methods(http.MethodGet)
 
 	return router
 }
